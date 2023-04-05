162c52f666af59f887dda9901d77de37745c91ac
baiqameliasurouyani <129942397+baiqameliasurouyani@users.noreply.github.com>
2023-04-05 01:43:23 -0700

    Add files via upload

---
+++ tugas pyhton 1.py
@@ -0,0 +1,26 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\USER\AppData\Local\Programs\Python\Python311\tugas pyhton 1.py
+daftar nilai mahasiswa:  (25, 90, 40, 95, 35, 70, 50, 20, 80, 65)
+nilai terbesar mahasiswa: 25
+daftar nilai mahasiswa:  (25, 90, 40, 95, 35, 70, 50, 20, 80, 65)
+nilai terbesar mahasiswa: 90
+daftar nilai mahasiswa:  (25, 90, 40, 95, 35, 70, 50, 20, 80, 65)
+nilai terbesar mahasiswa: 90
+daftar nilai mahasiswa:  (25, 90, 40, 95, 35, 70, 50, 20, 80, 65)
+nilai terbesar mahasiswa: 95
+daftar nilai mahasiswa:  (25, 90, 40, 95, 35, 70, 50, 20, 80, 65)
+nilai terbesar mahasiswa: 95
+daftar nilai mahasiswa:  (25, 90, 40, 95, 35, 70, 50, 20, 80, 65)
+nilai terbesar mahasiswa: 95
+daftar nilai mahasiswa:  (25, 90, 40, 95, 35, 70, 50, 20, 80, 65)
+nilai terbesar mahasiswa: 95
+daftar nilai mahasiswa:  (25, 90, 40, 95, 35, 70, 50, 20, 80, 65)
+nilai terbesar mahasiswa: 95
+daftar nilai mahasiswa:  (25, 90, 40, 95, 35, 70, 50, 20, 80, 65)
+nilai terbesar mahasiswa: 95
+daftar nilai mahasiswa:  (25, 90, 40, 95, 35, 70, 50, 20, 80, 65)
+nilai terbesar mahasiswa: 95
+>>> 25
+25
---
+++ while.py
@@ -0,0 +1,25 @@
+i = 0
+while i < 7:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+
+i = 9
+while i < 15:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+1
+2
+4
+5
+6
+7
+10
+11
+12
+13
+14
+15
